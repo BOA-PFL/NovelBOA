9731f9c5c40e575980b09024ae2b0009bbc3d3bf
Dan Feeney <daniel.feeney@colorado.edu>
2021-02-22 15:26:08 -0700

    SPM at three time points

--- Python/spm2d.py
+++ Python/spm2d.py
@@ -147,35 +147,50 @@ for landing in landings2:
 ### Formatting data into spm requirements
 
 # These need to be the same dimensions #
-if len(HSarray) < len(HSarray2):
-    correctLengthHS = len(HSarray)
-else:
-    correctLengthHS = len(HSarray2)
 
-stackedHSData = np.zeros((correctLengthHS,15,7))
-rowIndex = 0
-for row in HSarray:
-    if rowIndex < correctLengthHS:
-        tmpArray = row
-        stackedHSData[rowIndex,:,:] = np.flipud(reshapeArray(row))
-        rowIndex = rowIndex + 1
+def findRightLength(array1, array2):
+    # input the arrays 1 and 2 from the two conditions an find the shorter ##
+    # Array to make them the same shape (required for later in spm)
+    if len(array1) < len(array2):
+        correctLength = len(array1)
     else:
-        print('skipped row')
+        correctLength = len(array2)
+    return(correctLength)
     
-# Dataset 2
-stackedHSData2 = np.zeros((correctLengthHS,15,7))
-rowIndex = 0
-for row in HSarray2:
-    if rowIndex < correctLengthHS:
-        tmpArray = row
-        stackedHSData2[rowIndex,:,:] = np.flipud(reshapeArray(row))
-        rowIndex = rowIndex + 1
-    else:
-        print('skipped row')
-    
-### plotting average value
-### plotting subsets of data
+def calcStackedArray(inputArray, correctArrayLen):
+    ### Used to input array data in list format and output matrix ###
+    ### in a 3D format for SPM ###
+    preallocArray = np.zeros((correctLengthHS,15,7))
+    rowIndex = 0
+    for row in inputArray:
+        if rowIndex < correctArrayLen:
+            preallocArray[rowIndex,:,:] = np.flipud(reshapeArray(row))
+            rowIndex = rowIndex + 1
+        else:
+            print('skipped row')
+    return preallocArray
 
+# Heel Strike Data
+correctLengthHS = findRightLength(HSarray, HSarray2)
+stackedHSData = calcStackedArray(HSarray, correctLengthHS)
+stackedHSData2 = calcStackedArray(HSarray2, correctLengthHS)
+stackedHSDorsal = calcStackedArray(HSdorsal, correctLengthHS)
+stackedHSDorsal2 = calcStackedArray(HSdorsal2, correctLengthHS)
+# MS Data
+correctLengthMS = findRightLength(MSarray, MSarray2)
+stackedMSData = calcStackedArray(MSarray, correctLengthMS)
+stackedMSData2 = calcStackedArray(MSarray2, correctLengthMS)
+stackedMSDorsal = calcStackedArray(MSdorsal, correctLengthMS)
+stackedMSDorsal2 = calcStackedArray(MSdorsal2, correctLengthMS)
+# TO Data
+correctLengthTO = findRightLength(TOarray, TOarray2)
+stackedTOData = calcStackedArray(TOarray, correctLengthTO)
+stackedTOData2 = calcStackedArray(TOarray2, correctLengthTO)
+stackedTODorsal = calcStackedArray(TOdorsal, correctLengthTO)
+stackedTODorsal2 = calcStackedArray(TOdorsal2, correctLengthTO)
+
+### plotting average value for Heel Strike ######
+###############################################################################
 ## compute mean of two subsets of data
 mA  = stackedHSData.mean(axis=0)
 mB  = stackedHSData2.mean(axis=0)
@@ -232,6 +247,322 @@ plt.figure()
 ax = plt.axes()
 ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
 ax.set_title('SPM results at Heel Strike')
+plt.axvline(x=6.5)
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('t value')
+plt.show()
+
+###############################################################################
+### Heel Strike Dorsal #####
+### plotting average value for Heel Strike
+
+## compute mean of two subsets of data
+mA  = stackedHSDorsal.mean(axis=0)
+mB  = stackedHSDorsal2.mean(axis=0)
+m   = np.hstack( [mA, mB] )
+
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(m, m==0), origin='lower', cmap='jet')
+ax.set_title('Mean pressure distributions Heel Strike Dorsal')
+plt.axvline(x = 6.5, color = 'r')
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('Average pressure (kPa)')
+plt.show()
+
+### Statistical testing ###
+y       = np.array([yy.flatten() for yy in stackedHSDorsal])
+J,Q     = y.shape
+y2       = np.array([yy.flatten() for yy in stackedHSDorsal2])
+J2,Q2     = y2.shape
+#separate the observations:
+yA      = y #flattend dataset 1
+yB      = y2  #flattend observations for Task B
+
+#find zero-variance nodes:
+eps     = np.finfo(float).eps     #smallest float number
+iA      = yA.std(axis=0) > eps    #Task A indices where variance > 0
+iB      = yB.std(axis=0) > eps    #Task B indices where variance > 0
+i       = np.logical_and(iA, iB)  #indices where variance > 0 for both tasks
+
+ynz     = y[:,i]    #all observations with non-zero variance nodes removed
+ynzA    = ynz[:10]  #Task A observations with non-zero variance nodes removed
+ynzB    = ynz[10:]  #Task B observations with non-zero variance nodes removed
+
+# Run the tests
+snpm    = spm1d.stats.nonparam.ttest2(ynzA, ynzB)
+snpmi   = snpm.inference(0.05, two_tailed=True, iterations=10000)
+
+# Reshaping for visualization
+znz     = snpmi.z      #flattened test statistic  (i.e., t value) over only non-zero-variance nodes
+zstar   = snpmi.zstar  #critical test statistic  (i.e., critical t value)
+
+z       = np.zeros(Q)  #flattened test statistic including all nodes
+z[i]    = znz          #add test statistic values for the non-zero-variance nodes
+
+Z       = np.reshape(z, stackedHSDorsal.shape[1:])   #2D test statistic image
+Zi      = Z.copy()                     #2D inference image (temporary)
+Zi[np.abs(Z)<zstar] = 0                #thresholded test statistic image
+
+ZZ      = np.hstack( [Z, Zi] )         #both raw test statistic image and inference image
+
+
+#plot:
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
+ax.set_title('SPM results at Heel Strike Dorsal Side')
+plt.axvline(x=6.5)
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('t value')
+plt.show()
+
+###############################################################################
+## Mid Stance Plantar
+## compute mean of two subsets of data
+mA  = stackedMSData.mean(axis=0)
+mB  = stackedMSData2.mean(axis=0)
+m   = np.hstack( [mA, mB] )
+
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(m, m==0), origin='lower', cmap='jet')
+ax.set_title('Mean pressure Mid Stance Plantar')
+plt.axvline(x = 6.5, color = 'r')
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('Average pressure (kPa)')
+plt.show()
+
+### Statistical testing ###
+y       = np.array([yy.flatten() for yy in stackedMSData])
+J,Q     = y.shape
+y2       = np.array([yy.flatten() for yy in stackedMSData2])
+J2,Q2     = y2.shape
+#separate the observations:
+yA      = y #flattend dataset 1
+yB      = y2  #flattend observations for Task B
+
+#find zero-variance nodes:
+eps     = np.finfo(float).eps     #smallest float number
+iA      = yA.std(axis=0) > eps    #Task A indices where variance > 0
+iB      = yB.std(axis=0) > eps    #Task B indices where variance > 0
+i       = np.logical_and(iA, iB)  #indices where variance > 0 for both tasks
+
+ynz     = y[:,i]    #all observations with non-zero variance nodes removed
+ynzA    = ynz[:10]  #Task A observations with non-zero variance nodes removed
+ynzB    = ynz[10:]  #Task B observations with non-zero variance nodes removed
+
+# Run the tests
+snpm    = spm1d.stats.nonparam.ttest2(ynzA, ynzB)
+snpmi   = snpm.inference(0.05, two_tailed=True, iterations=10000)
+
+# Reshaping for visualization
+znz     = snpmi.z      #flattened test statistic  (i.e., t value) over only non-zero-variance nodes
+zstar   = snpmi.zstar  #critical test statistic  (i.e., critical t value)
+
+z       = np.zeros(Q)  #flattened test statistic including all nodes
+z[i]    = znz          #add test statistic values for the non-zero-variance nodes
+
+Z       = np.reshape(z, stackedMSData.shape[1:])   #2D test statistic image
+Zi      = Z.copy()                     #2D inference image (temporary)
+Zi[np.abs(Z)<zstar] = 0                #thresholded test statistic image
+
+ZZ      = np.hstack( [Z, Zi] )         #both raw test statistic image and inference image
+
+
+#plot:
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
+ax.set_title('SPM results at Mid Stance')
+plt.axvline(x=6.5)
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('t value')
+plt.show()
+
+###############################################################################
+## Mid Stance Dorsal
+## compute mean of two subsets of data
+mA  = stackedMSDorsal.mean(axis=0)
+mB  = stackedMSDorsal2.mean(axis=0)
+m   = np.hstack( [mA, mB] )
+
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(m, m==0), origin='lower', cmap='jet')
+ax.set_title('Mean pressure distribution Mid Stance Dorsal')
+plt.axvline(x = 6.5, color = 'r')
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('Average pressure (kPa)')
+plt.show()
+
+### Statistical testing ###
+y       = np.array([yy.flatten() for yy in stackedMSDorsal])
+J,Q     = y.shape
+y2       = np.array([yy.flatten() for yy in stackedMSDorsal2])
+J2,Q2     = y2.shape
+#separate the observations:
+yA      = y #flattend dataset 1
+yB      = y2  #flattend observations for Task B
+
+#find zero-variance nodes:
+eps     = np.finfo(float).eps     #smallest float number
+iA      = yA.std(axis=0) > eps    #Task A indices where variance > 0
+iB      = yB.std(axis=0) > eps    #Task B indices where variance > 0
+i       = np.logical_and(iA, iB)  #indices where variance > 0 for both tasks
+
+ynz     = y[:,i]    #all observations with non-zero variance nodes removed
+ynzA    = ynz[:10]  #Task A observations with non-zero variance nodes removed
+ynzB    = ynz[10:]  #Task B observations with non-zero variance nodes removed
+
+# Run the tests
+snpm    = spm1d.stats.nonparam.ttest2(ynzA, ynzB)
+snpmi   = snpm.inference(0.05, two_tailed=True, iterations=10000)
+
+# Reshaping for visualization
+znz     = snpmi.z      #flattened test statistic  (i.e., t value) over only non-zero-variance nodes
+zstar   = snpmi.zstar  #critical test statistic  (i.e., critical t value)
+
+z       = np.zeros(Q)  #flattened test statistic including all nodes
+z[i]    = znz          #add test statistic values for the non-zero-variance nodes
+
+Z       = np.reshape(z, stackedMSDorsal.shape[1:])   #2D test statistic image
+Zi      = Z.copy()                     #2D inference image (temporary)
+Zi[np.abs(Z)<zstar] = 0                #thresholded test statistic image
+
+ZZ      = np.hstack( [Z, Zi] )         #both raw test statistic image and inference image
+
+
+#plot:
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
+ax.set_title('SPM results at Mid Stance Dorsal')
+plt.axvline(x=6.5)
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('t value')
+plt.show()
+
+################################################################################
+## Toe off Plantar
+## compute mean of two subsets of data
+mA  = stackedTOData.mean(axis=0)
+mB  = stackedTOData2.mean(axis=0)
+m   = np.hstack( [mA, mB] )
+
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(m, m==0), origin='lower', cmap='jet')
+ax.set_title('Mean pressure distributions')
+plt.axvline(x = 6.5, color = 'r')
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('Average pressure (kPa)')
+plt.show()
+
+### Statistical testing ###
+y       = np.array([yy.flatten() for yy in stackedTOData])
+J,Q     = y.shape
+y2       = np.array([yy.flatten() for yy in stackedTOData2])
+J2,Q2     = y2.shape
+#separate the observations:
+yA      = y #flattend dataset 1
+yB      = y2  #flattend observations for Task B
+
+#find zero-variance nodes:
+eps     = np.finfo(float).eps     #smallest float number
+iA      = yA.std(axis=0) > eps    #Task A indices where variance > 0
+iB      = yB.std(axis=0) > eps    #Task B indices where variance > 0
+i       = np.logical_and(iA, iB)  #indices where variance > 0 for both tasks
+
+ynz     = y[:,i]    #all observations with non-zero variance nodes removed
+ynzA    = ynz[:10]  #Task A observations with non-zero variance nodes removed
+ynzB    = ynz[10:]  #Task B observations with non-zero variance nodes removed
+
+# Run the tests
+snpm    = spm1d.stats.nonparam.ttest2(ynzA, ynzB)
+snpmi   = snpm.inference(0.05, two_tailed=True, iterations=10000)
+
+# Reshaping for visualization
+znz     = snpmi.z      #flattened test statistic  (i.e., t value) over only non-zero-variance nodes
+zstar   = snpmi.zstar  #critical test statistic  (i.e., critical t value)
+
+z       = np.zeros(Q)  #flattened test statistic including all nodes
+z[i]    = znz          #add test statistic values for the non-zero-variance nodes
+
+Z       = np.reshape(z, stackedTOData.shape[1:])   #2D test statistic image
+Zi      = Z.copy()                     #2D inference image (temporary)
+Zi[np.abs(Z)<zstar] = 0                #thresholded test statistic image
+
+ZZ      = np.hstack( [Z, Zi] )         #both raw test statistic image and inference image
+
+
+#plot:
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
+ax.set_title('SPM results at Mid Stance')
+plt.axvline(x=6.5)
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('t value')
+plt.show()
+###############################################################################
+## Toe off Dorsal
+## compute mean of two subsets of data
+mA  = stackedTODorsal.mean(axis=0)
+mB  = stackedTODorsal2.mean(axis=0)
+m   = np.hstack( [mA, mB] )
+
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(m, m==0), origin='lower', cmap='jet')
+ax.set_title('Mean pressure distribution Toe Off Dorsal')
+plt.axvline(x = 6.5, color = 'r')
+cb = plt.colorbar(mappable=ax.images[0])
+cb.set_label('Average pressure (kPa)')
+plt.show()
+
+### Statistical testing ###
+y       = np.array([yy.flatten() for yy in stackedTODorsal])
+J,Q     = y.shape
+y2       = np.array([yy.flatten() for yy in stackedTODorsal2])
+J2,Q2     = y2.shape
+#separate the observations:
+yA      = y #flattend dataset 1
+yB      = y2  #flattend observations for Task B
+
+#find zero-variance nodes:
+eps     = np.finfo(float).eps     #smallest float number
+iA      = yA.std(axis=0) > eps    #Task A indices where variance > 0
+iB      = yB.std(axis=0) > eps    #Task B indices where variance > 0
+i       = np.logical_and(iA, iB)  #indices where variance > 0 for both tasks
+
+ynz     = y[:,i]    #all observations with non-zero variance nodes removed
+ynzA    = ynz[:10]  #Task A observations with non-zero variance nodes removed
+ynzB    = ynz[10:]  #Task B observations with non-zero variance nodes removed
+
+# Run the tests
+snpm    = spm1d.stats.nonparam.ttest2(ynzA, ynzB)
+snpmi   = snpm.inference(0.05, two_tailed=True, iterations=10000)
+
+# Reshaping for visualization
+znz     = snpmi.z      #flattened test statistic  (i.e., t value) over only non-zero-variance nodes
+zstar   = snpmi.zstar  #critical test statistic  (i.e., critical t value)
+
+z       = np.zeros(Q)  #flattened test statistic including all nodes
+z[i]    = znz          #add test statistic values for the non-zero-variance nodes
+
+Z       = np.reshape(z, stackedTODorsal.shape[1:])   #2D test statistic image
+Zi      = Z.copy()                     #2D inference image (temporary)
+Zi[np.abs(Z)<zstar] = 0                #thresholded test statistic image
+
+ZZ      = np.hstack( [Z, Zi] )         #both raw test statistic image and inference image
+
+
+#plot:
+plt.figure()
+ax = plt.axes()
+ax.imshow(np.ma.masked_array(ZZ, ZZ==0), origin='lower', cmap='jet', vmin=-15, vmax=15)
+ax.set_title('SPM results at Toe Off Dorsal')
 plt.axvline(x=6.5)
 cb = plt.colorbar(mappable=ax.images[0])
 cb.set_label('t value')
